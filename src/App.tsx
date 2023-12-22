0ae128646e7252753938f13e9183457831769b69
Sam Magdaleno <smagdaleno@cetys.edu.mx>
2023-12-21 20:03:15 -0800

    Code - Freeze, must setup server first.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Callback from './services/spotify.login.service';
 import Home from "./pages/home";
 import './styles/App.css';
 
 const App: React.FC = () => {
-  return(
-    <Router>
-      <Routes>
-        <Route path="/callback" Component={Callback} />
-        <Route path="/" Component={Home}/>
-      </Routes>
-    </Router>
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" Component={Home} />
+        </Routes>
+      </Router>
+    </div>
   );
 };
 
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,36 +1,48 @@
 import React from "react";
-import { Link, Navigate } from "react-router-dom";
 import tokenService from "../services/token.service";
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 const Home: React.FC = () => {
 
-    const clientId = "b2b03bd7211e4c189a77861c11fb4b85"; // TODO: Replace with secret or environmental variable
+    const clientId = "NOPE"; // TODO: Replace with secret or environmental variable
     const redirectUri = "http://localhost:3000"; // TODO: Replace with actual redirect URI
     const scopes = 'user-read-private user-read-email'; // Specify the scopes you need
-  
+    let isLoggedIn: boolean;
+
     const spotifyLoginUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=token&scope=${encodeURIComponent(scopes)}`;
-  
-    // const getAccessTokenFromUrl = () => {
-    //     const match = window.location.hash.match(/#access_token=([^&]*)/);
-    //     return match && match[1];
-    // };
+     // Ya vi porque es peligros, no tengo un servidor intermedio para manejar las llamadas a si mismo 
+     // Este me regresa una navegacion
 
     if (window.location.hash.includes('access_token')) {
         tokenService.setAccessTokenFromUrl(window.location.toString());
-        return <Navigate to="/" />;
     }
-    
-    if(tokenService.getAccessToken()){
-        alert(tokenService.getAccessToken());
-    }else{
-        console.log("FUCKYOU!");
+
+    if (!tokenService.getAccessToken()) {
+        isLoggedIn = true;
+        console.log("No token yet!");
+    } else {
+        isLoggedIn = false;
+        console.log("You've got the token in you, boy.");
     }
-    
-    return(
-        <div>
-            <h1>Spotify Login Fucker</h1>
-            <Link to={spotifyLoginUrl}>Log in with Spotify</Link>
-        </div>
+
+
+    return (
+        <React.Fragment>
+            <Container maxWidth="sm" sx={{alignContent: 'center'}}>
+                <Box sx={{ bgcolor: 'transparent', alignContent: 'center'}}>
+                    <h1>Spotify Login Fucker</h1>
+                    {
+                        isLoggedIn ? (
+                            <Button variant="contained" href={spotifyLoginUrl}> Login to Spotify, dummy </Button> 
+                        ) : (
+                            <Button variant="contained" href={"/"}> Let the magic, start!</Button>
+                        )
+                    }
+                </Box>
+            </Container>
+        </React.Fragment>
     );
 };
 

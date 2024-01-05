de5646b2fe4bbc37bacbb3a16fe6c5e6472dcf7a
Sam Magdaleno <smagdaleno@cetys.edu.mx>
2024-01-05 09:09:00 -0800

    Added redirect file.

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from "./pages/home";
 import About from "./pages/about";
 import SpotifyCallback from './components/navigation/spotifyCallback';
-// import SpotifyAuth from './components/navigation/spotifyAuth';
+import SpotifyAuth from './components/navigation/spotifyAuth';
 import './styles/App.css';
 import Layout from './components/layout/layout';
-import Login from './pages/login';
+// import Login from './pages/login';
 
 function App ()  {
   return (
@@ -15,7 +15,7 @@ function App ()  {
         <Routes>
           <Route path="/" Component={Home} />
           <Route path="/callback" Component={SpotifyCallback} />
-          <Route path="/login" Component={Login} />
+          <Route path="/login" Component={SpotifyAuth} />
           <Route path="/about" Component={About} />
         </Routes>
       </Layout>

5b75dadc1acb41fef6e41adcf43140f11bcd4571
Sam Magdaleno <smagdaleno@cetys.edu.mx>
2024-01-15 18:40:57 -0800

    Fixed react dom warning, although not completely satisfied with how it turned out.

--- src/components/layout/landingPageLayout.tsx
+++ src/components/layout/landingPageLayout.tsx
@@ -1,23 +1,17 @@
 import React, { useState } from "react";
-import { Avatar, Box, Chip, Stack, ToggleButtonGroup, styled } from "@mui/material";
+import { Avatar, Box, Chip, Stack, ToggleButtonGroup } from "@mui/material";
 import UserData from "../../interfaces/user.data";
 import { getLSArtistsData, getLSTracksListData, getLSUserData } from "../../repos/spotify.repo";
 import './landingPageLayout.css';
 import TrackAnalysisTab from "../trackAnalysisTab/trackAnalysisTab";
 import Track from "../../interfaces/spotify/track";
 
-// TODO: CHECK THIS STYLE
-const StyledChip = styled(Chip) <{ isselected: boolean }>` 
-    background-color: ${(props) => (props.isselected ? '#10eb60' : '#2A2A2A')};
-    color: ${(props) => (props.isselected ? '#002A07' : 'inherit')};
-`;
-
 export default function LandingPageLayout() {
-    
+
     const [selectedChip, setSelectedChip] = useState<number | null>(0);
-    const [selectedTimeframe, setSelectedTimeframe] = useState<string>("short_term"); 
+    const [selectedTimeframe, setSelectedTimeframe] = useState<string>("short_term");
     const [selectedTrackList, setSelectedTrackList] = useState<Track[]>(getLSTracksListData("short_term"));
-    const userData: UserData = getLSUserData(); 
+    const userData: UserData = getLSUserData();
     // console.log(selectedTrackList);
 
     const longTermArtistList = getLSArtistsData("long_term");
@@ -37,44 +31,35 @@ export default function LandingPageLayout() {
 
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-            <Stack direction="row" spacing={2} alignItems="center" sx={{pb:1}} >
+            <Stack direction="row" spacing={2} alignItems="center" sx={{ pb: 1 }} >
                 <Avatar alt="Remy Sharp" src={userData.imageUrl} />
                 <ToggleButtonGroup size="large" aria-label="Large sizes">
                     <Stack direction="row" spacing={1}>
-                        <StyledChip
-                            isselected={selectedChip === 0}
+                        <Chip
                             className="chip-toggle-button"
                             label="4 Weeks"
                             onClick={() => handleClick(0, "short_term")}
-                        />
-                        <StyledChip
-                            isselected={selectedChip === 1}
+                            sx={{
+                                color: selectedChip === 0 ? '#002A07' : 'inherit',
+                                backgroundColor: selectedChip === 0 ? '#10eb60' : '2A2A2A',
+                            }} />
+                        <Chip
                             className="chip-toggle-button"
                             label="6 Months"
-                            onClick={() => handleClick(1, "medium_term")}/>
-                        <StyledChip 
-                            isselected={selectedChip === 2}
+                            onClick={() => handleClick(1, "medium_term")} 
+                            sx={{
+                                color: selectedChip === 1 ? '#002A07' : 'inherit',
+                                backgroundColor: selectedChip === 1 ? '#10eb60' : '2A2A2A',
+                            }}
+                        />
+                        <Chip
                             className="chip-toggle-button"
                             label="All time"
-                            onClick={() => handleClick(2, "long_term")}/>
-                        {/* <StyledChip
-                            isselected={selectedChip === 0}
-                            className="chip-toggle-button"
-                            label="All time"
-                            onClick={() => handleClick(0, "long_term")}
-                        />
-                        <StyledChip
-                            isselected={selectedChip === 1}
-                            className="chip-toggle-button"
-                            label="6 Months"
-                            onClick={() => handleClick(1, "medium_term")}
-                        />
-                        <StyledChip
-                            isselected={selectedChip === 2}
-                            className="chip-toggle-button"
-                            label="4 Weeks"
-                            onClick={() => handleClick(2, "short_term")}
-                        /> */}
+                            onClick={() => handleClick(2, "long_term")} 
+                            sx={{
+                                color: selectedChip === 2 ? '#002A07' : 'inherit',
+                                backgroundColor: selectedChip === 2 ? '#10eb60' : '2A2A2A',
+                            }}/>
                     </Stack>
                 </ToggleButtonGroup>
             </Stack>
